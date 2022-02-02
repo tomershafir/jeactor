21e003e8ad41dfe90c681abb251973c28342ef60
‘tomershafir’ <tomer.shafir8@gmail.com>
2022-02-02 12:07:40 +0200

    Created registry package and registrar abstraction

---
+++ src/main/java/com/jeactor/registry/Registrar.java
@@ -0,0 +1,25 @@
+package com.jeactor.registry;
+
+/**
+ * An interface that represents an object that supports registration of values to keys.
+ * 
+ * @param <K> type of keys to which values are registered
+ * @param <V> type of values to be registered
+ */
+public interface Registrar<K, V> {
+    /**
+     * The method registers the accepted value with the accepted key.
+     * 
+     * @param key a key to which the accepted value is registered
+     * @param value a value to register
+     */
+    public abstract void register(K key, V value);
+
+    /**
+     * The method unregisters the accepted value with the accepted key.
+     * 
+     * @param key a key from which the accepted value is unregistered
+     * @param value a value to unregister
+     */
+    public abstract void unregister(K key, V value);
+}
