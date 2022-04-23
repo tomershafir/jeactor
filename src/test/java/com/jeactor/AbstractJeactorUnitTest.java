c2342175e079bd66d33702adf93f7ae8f1a3d384
tomershafir <tomer.shafir8@gmail.com>
2022-04-23 15:38:11 +0300

    Concrete for jeactor base unit test class

---
+++ src/test/java/com/jeactor/AbstractJeactorUnitTest.java
@@ -0,0 +1,13 @@
+package com.jeactor;
+
+/**
+ * Abstract jeactor unit test class.
+ * 
+ * <p> All jeactor's unit test classes should extend this class.
+ */
+public abstract class AbstractJeactorUnitTest extends AbstractUnitTest {
+    /**
+     * Creates default jeactor unit test instance.
+     */
+    protected AbstractJeactorUnitTest() {}
+}
